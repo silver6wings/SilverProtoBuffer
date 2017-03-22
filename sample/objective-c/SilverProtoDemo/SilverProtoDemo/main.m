3040d582d544a24217b78c12cb6017a3c1a08f59
silver6wings <silver6wings@gmail.com>
2017-03-22 19:21:27 +0800

    Add sample

---
+++ sample/objective-c/SilverProtoDemo/SilverProtoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SilverProtoDemo
+//
+//  Created by JUNCHAO on 2017/3/22.
+//  Copyright © 2017年 silver6wings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
